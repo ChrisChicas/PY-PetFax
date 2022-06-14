4f82c95c75f92528ebb97e534b9ce6e2ed193f41
Chris Chicas <christianchicas7@gmail.com>
2022-06-13 22:44:36 -0400

    added post method for facts and connection to postgres

--- project/__init__.py
+++ project/__init__.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from flask_migrate import Migrate
 
 def create_app():
     app = Flask(__name__)
 
+    from . import _config
+    app.config['SQLALCHEMY_DATABASE_URI'] = _config.connection_string
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    from . import models
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
+
     @app.route("/")
-    def hello():
+    def index():
         return "Hello, PetFax!"
     
     from . import (pet, fact)
--- project/pet.py
+++ project/pet.py
@@ -11,5 +11,5 @@ def index():
 
 @bp.route("/<int:id>")
 def show(id):
-    pet = pets[id]
+    pet = pets[id - 1]
     return render_template("pets/show.html", pet=pet)
